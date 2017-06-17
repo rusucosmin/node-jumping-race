31676b25bc8779abdf1f422ea81b300976fff206
Cosmin <cr.rusucosmin@gmail.com>
2017-06-17 19:06:32 +0300

    added forward / backward

--- index.js
+++ index.js
@@ -3,21 +3,64 @@ const drone=new parrot.Wifi;
 
 drone.connect();
 
+function turnRight() {
+  console.log("turnRight()");
+
+}
+
+function turnLeft() {
+  console.log("turnLeft()");
+}
+
+function createPCMDMessage(project, commandClass) {
+  return drone.message.build(
+    project.info.id,
+    commandClass.info.id,
+    commandClass.PCMD
+  );
+}
+
+function forward() {
+  console.log("forward()");
+  const project=drone.projects.jpsumo;
+  const commandClass=project.Piloting;
+
+  commandClass.PCMD.flag.value = 1;
+  commandClass.PCMD.speed.value = 100;
+  commandClass.PCMD.turn.value = 0;
+
+  const PCMDMessage = createPCMDMessage(project, commandClass);
+  drone.message.send(PCMDMessage);
+}
+
+function backward() {
+  console.log("backward()");
+  const project=drone.projects.jpsumo;
+  const commandClass=project.Piloting;
+
+  commandClass.PCMD.flag.value = 1;
+  commandClass.PCMD.speed.value = -50;
+  commandClass.PCMD.turn.value = 0;
+
+  const PCMDMessage = createPCMDMessage(project, commandClass);
+  drone.message.send(PCMDMessage);
+}
+
 drone.on(
   'connected',
   function(){
     console.log('CONNECTED\n\n')
 
-    setInterval(
-       move,
-       1000
-    );
+    setInterval(function(){
+      forward();
+      //backward();
+    }, 1000);
 
     const project=drone.projects.jpsumo;
     const commandClass=project.Piloting;
 
     //change the value of the args you want to change if applicable
-    commandClass.Posture.type.value=0;
+    commandClass.Posture.type.value=1;
 
     console.log(commandClass.info);
 
