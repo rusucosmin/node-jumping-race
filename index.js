357e1995f4be4f709105cfb914ef550ffe78deaa
Cosmin <cr.rusucosmin@gmail.com>
2017-06-20 11:47:29 +0300

    added turn left, turn right and tied arrow controls to arrow keys

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const parrot=require('node-parrot-drone');
+const keypress = require('keypress')
 const drone=new parrot.Wifi;
 
+///start keypress
+keypress(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  console.log('got "keypress"', key);
+  if (key && key.name == 'up') {
+    forward();
+  } else if (key && key.name == 'down') {
+    backward();
+  } else if (key && key.name == 'right') {
+    turnRight();
+  } else if (key && key.name == 'left') {
+    turnLeft();
+  }
+  if (key && key.ctrl && key.name == 'c') {
+    process.exit();
+  }
+});
+
 drone.connect();
-
-function turnRight() {
-  console.log("turnRight()");
-
-}
-
-function turnLeft() {
-  console.log("turnLeft()");
-}
 
 function createPCMDMessage(project, commandClass) {
   return drone.message.build(
@@ -46,79 +60,35 @@ function backward() {
   drone.message.send(PCMDMessage);
 }
 
+function turnRight() {
+  console.log("turnRight()");
+  const project = drone.projects.jpsumo;
+  const commandClass = project.Piloting;
+
+  commandClass.PCMD.flag.value = 1;
+  commandClass.PCMD.speed.value = 100;
+  commandClass.PCMD.turn.value = 25;
+
+  const msg = createPCMDMessage(project, commandClass);
+  drone.message.send(msg);
+}
+
+function turnLeft() {
+  console.log("turnLeft()");
+  const project = drone.projects.jpsumo;
+  const commandClass = project.Piloting;
+
+  commandClass.PCMD.flag.value = 1;
+  commandClass.PCMD.speed.value = 100;
+  commandClass.PCMD.turn.value = -25;
+
+  const msg = createPCMDMessage(project, commandClass);
+  drone.message.send(msg);
+}
+
 drone.on(
   'connected',
   function(){
     console.log('CONNECTED\n\n')
-
-    setInterval(function(){
-      forward();
-      //backward();
-    }, 1000);
-
-    const project=drone.projects.jpsumo;
-    const commandClass=project.Piloting;
-
-    //change the value of the args you want to change if applicable
-    commandClass.Posture.type.value=1;
-
-    console.log(commandClass.info);
-
-    //build a message requesting all settings
-    const MoveMessage=drone.message.build(
-      project.info.id,
-      commandClass.info.id,
-      commandClass.Posture
-    );
-    drone.message.send(MoveMessage);
-/*
-    setInterval(
-      flip,
-      1000
-    );
-    */
-  }
-);
-
-function move(){
-  const project=drone.projects.jpsumo;
-  const commandClass=project.Piloting;
-
-  //change the value of the args you want to change if applicable
-  commandClass.PCMD.flag.value=1;
-  commandClass.PCMD.speed.value=-100;
-  commandClass.PCMD.turn.value=0;
-
-  //build a message requesting all settings
-  const MoveMessage=drone.message.build(
-    project.info.id,
-    commandClass.info.id,
-    commandClass.PCMD
-  );
-  drone.message.send(MoveMessage);
-}
-
-function flip(){
-  const project=drone.projects.jpsumo;
-  const commandClass=project.Piloting;
-
-  //change the value of the args you want to change if applicable
-  commandClass.Posture.type.value=Math.round(
-    Math.random()*3
-  );
-
-  //build a message requesting all settings
-  const MoveMessage=drone.message.build(
-    project.info.id,
-    commandClass.info.id,
-    commandClass.Posture
-  );
-  drone.message.send(MoveMessage);
-}
-
-drone.on(
-  'SpeedChanged',
-  function(data){
-    console.log(data)
   }
 );
